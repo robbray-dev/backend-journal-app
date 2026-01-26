d86819150a94093d2fbd48b094fd678a0da0b0ce
Robert Bray <rbray77777@gmail.com>
2026-01-26 12:51:00 -0500

    did the range of dates endpoint

--- src/main/java/com/journal_app/backend_journal_app/services/IEntriesService.java
+++ src/main/java/com/journal_app/backend_journal_app/services/IEntriesService.java
@@ -3,6 +3,7 @@ package com.journal_app.backend_journal_app.services;
 import com.journal_app.backend_journal_app.dto.EntriesDto;
 import com.journal_app.backend_journal_app.model.Entries;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface IEntriesService {
@@ -16,6 +17,8 @@ public interface IEntriesService {
     EntriesDto updateEntry(Long id, EntriesDto entry);
 
     List<EntriesDto> getTodaysEntries(String personId);
+
+    List<EntriesDto> getRangeOfEntries(String personId, LocalDate start, LocalDate end);
 
 
 
